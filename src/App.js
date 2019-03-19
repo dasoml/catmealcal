20b503d9f3e9c3d9ba5187918c78c8f75523e473
DasomL <somgrapik@gmail.com>
2019-03-19 23:53:26 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
     });
   };
   componentDidMount() {
-    fetch("http://10.0.1.13:8000/api/cat/")
+    fetch("http://10.0.1.13:8000/api/")
       .then(res => res.json())
       .then(json => {
         this.setState({ data: json });
