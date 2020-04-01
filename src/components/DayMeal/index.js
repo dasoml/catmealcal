c15c01e7b1d3ea5e6dd0538d234a1abd202eb968
dasoml <somgrapik@gmail.com>
2020-04-01 13:20:52 +0900

    test

--- src/components/DayMeal/index.js
+++ src/components/DayMeal/index.js
@@ -7,7 +7,7 @@ class DayMeal extends Component {
       <div className={this.props.visible}>
         <hr />
         <div>
-          <span>{this.props.catName}</span>는 하루에
+          <span>{this.props.catName}</span>는 하루에 ㅇㅇㅇ
           <br /> <span>{this.props.dayMeal}g</span>의 사료를 섭취하면 좋겠어요!
         </div>
         <hr />
