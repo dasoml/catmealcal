daef096895893a0496fb7e3407c8288728701c24
dasoml <somgrapik@gmail.com>
2020-04-01 13:43:23 +0900

    결과값 반올림

--- src/components/DayMeal/index.js
+++ src/components/DayMeal/index.js
@@ -7,8 +7,8 @@ class DayMeal extends Component {
       <div className={this.props.visible}>
         <hr />
         <div>
-          <span>{this.props.catName}</span>는 하루에 ㅇㅇㅇ
-          <br /> <span>{this.props.dayMeal}g</span>의 사료를 섭취하면 좋겠어요!
+          <span>{this.props.catName}</span>는 하루에
+          <br /> <span>{Math.floor(this.props.dayMeal*100)/100}g</span>의 사료를 섭취하면 좋겠어요!
         </div>
         <hr />
       </div>
