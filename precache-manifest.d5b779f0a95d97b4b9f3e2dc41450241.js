4e6480ba80a1343ca27ec64610ec9de51d31d9e3
dasoml <somgrapik@gmail.com>
2020-04-01 11:30:44 +0900

    Updates

---
+++ precache-manifest.d5b779f0a95d97b4b9f3e2dc41450241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "422f39e537f8fd3f9906",
+    "url": "/catmealcal/static/css/main.fb9e853b.chunk.css"
+  },
+  {
+    "revision": "422f39e537f8fd3f9906",
+    "url": "/catmealcal/static/js/main.50858052.chunk.js"
+  },
+  {
+    "revision": "621a76a907798ec57d78",
+    "url": "/catmealcal/static/js/runtime~main.0296b714.js"
+  },
+  {
+    "revision": "0b6d74439d347d1615ab",
+    "url": "/catmealcal/static/js/2.2c811a6b.chunk.js"
+  },
+  {
+    "revision": "619a6f4afd46a3e17befeff59f0437d4",
+    "url": "/catmealcal/index.html"
+  }
+];
